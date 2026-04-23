201445304ee475cc65482426b5ff393c7013cce4
projectqijik-dev <projectqijik@gmail.com>
2026-04-24 01:06:13 +0300

    musicplayer

---
+++ muzik_verisi.js
@@ -0,0 +1,146 @@
+window.MUZIK_VERITABANI = [
+    {
+        kategoriAdi: "Mola",
+        kategoriIkon: "fa-compact-disc",
+        sarkilar: [
+            {
+                baslik: "Özgürlük",
+                sanatci: "Zülfü Livaneli",
+                sure: "03:45", 
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/%C3%96zg%C3%BCrl%C3%BCk.mp3"
+            },
+            {
+                baslik: "Ben Sana Vurgunum",
+                sanatci: "Ali Kocatepe",
+                sure: "04:00",
+                kapak: "fa-music", 
+                link: "https://senin-cloudflare-r2-linkin.com/odaklanma-sinav.mp3"
+            }
+        ]
+    },
+	    {
+        kategoriAdi: "Biraz Hareket",
+        kategoriIkon: "fa-compact-disc",
+        sarkilar: [
+            {
+                baslik: "Özgürlük",
+                sanatci: "Zülfü Livaneli",
+                sure: "03:45", 
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/%C3%96zg%C3%BCrl%C3%BCk.mp3"
+            },
+            {
+                baslik: "Ben Sana Vurgunum",
+                sanatci: "Ali Kocatepe",
+                sure: "04:00",
+                kapak: "fa-music", 
+                link: "https://senin-cloudflare-r2-linkin.com/odaklanma-sinav.mp3"
+            }
+        ]
+    },
+	    {
+        kategoriAdi: "Klasik",
+        kategoriIkon: "fa-compact-disc",
+        sarkilar: [
+            {
+                baslik: "Özgürlük",
+                sanatci: "Zülfü Livaneli",
+                sure: "03:45", 
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/%C3%96zg%C3%BCrl%C3%BCk.mp3"
+            },
+            {
+                baslik: "Ben Sana Vurgunum",
+                sanatci: "Ali Kocatepe",
+                sure: "04:00",
+                kapak: "fa-music", 
+                link: "https://senin-cloudflare-r2-linkin.com/odaklanma-sinav.mp3"
+            }
+        ]
+    },
+	
+    {
+        kategoriAdi: "Dinlenme & Odaklanma",
+        kategoriIkon: "fa-leaf",
+        sarkilar: [
+            {
+                baslik: "Piramitlerin Gölgesinde",
+                sanatci: "Enstrümantal",
+                sure: "04:21",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Ali%20Kocatepe%20-%20Ben%20Sana%20Vurgunum%20(1979).mp3"
+            },
+            {
+                baslik: "Nil Nehri'nin Sesi",
+                sanatci: "Doğa Sesleri",
+                sure: "10:00",
+                kapak: "fa-music", 
+                link: "https://senin-cloudflare-r2-linkin.com/nil-nehri.mp3"
+            }
+        ]
+    },
+		{
+        kategoriAdi: "Canlı Radyo",
+        kategoriIkon: "fa-radio",
+        sarkilar: [
+			{
+                baslik: "Radio Swiss Classic",
+                sanatci: "Klasik Müzik (İsviçre)",
+                sure: "Canlı",
+                kapak: "fa-satellite-dish", 
+                link: "https://stream.srg-ssr.ch/m/rsc_de/mp3_128"
+            },
+            {
+                baslik: "WQXR New York",
+                sanatci: "Klasik Müzik",
+                sure: "Canlı",
+                kapak: "fa-satellite-dish", 
+                link: "https://stream.wqxr.org/wqxr"
+            },
+            {
+                baslik: "Jazz24",
+                sanatci: "Smooth Jazz (Seattle)",
+                sure: "Canlı",
+                kapak: "fa-satellite-dish", 
+                link: "https://knkx-live-a.edge.audiocdn.com/6285_128k"
+            },
+            {
+                baslik: "Relaxing Jazz",
+                sanatci: "Lounge & Dinlenme",
+                sure: "Canlı",
+                kapak: "fa-satellite-dish", 
+                link: "https://stream-02-eu.relaxingjazz.com/stream/1/"
+            },
+			{
+                baslik: "Radyo Voyage",
+                sanatci: "Dünya Müzikleri & Etnik",
+                sure: "Canlı",
+                kapak: "fa-earth-europe", 
+                link: "https://playerservices.streamtheworld.com/api/livestream-redirect/VOYAGE.mp3"
+            },
+            {
+                baslik: "Borusan Klasik",
+                sanatci: "Klasik Müzik & Sanat",
+                sure: "Canlı",
+                kapak: "fa-compact-disc", 
+                link: "https://playerservices.streamtheworld.com/api/livestream-redirect/BORUSAN_KLASIK.mp3"
+            },
+            {
+                baslik: "Joy Türk",
+                sanatci: "Türkçe Akustik & Slow",
+                sure: "Canlı",
+                kapak: "fa-guitar", 
+                link: "https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_TURK128AAC.aac"
+            },
+            {
+                baslik: "Power Pop Akustik",
+                sanatci: "Sakin Türkçe Tınılar",
+                sure: "Canlı",
+                kapak: "fa-microphone-lines", 
+                link: "https://powerpop.listenpowerapp.com/powerpop/mpeg/icecast.audio"
+            }
+        ]
+		
+    },
+];
