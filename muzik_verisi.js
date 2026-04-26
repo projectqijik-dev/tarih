789ccae2af687bf7599712379d005ef9755dcc33
projectqijik-dev <projectqijik@gmail.com>
2026-04-27 00:26:32 +0300

    Update muzik_verisi.js

--- muzik_verisi.js
+++ muzik_verisi.js
@@ -6,17 +6,10 @@ window.MUZIK_VERITABANI = [
             {
                 baslik: "Özgürlük",
                 sanatci: "Zülfü Livaneli",
-                sure: "03:45", 
+                sure: "", 
                 kapak: "fa-music", 
                 link: "https://depo.kirkyama.uk/music/turkish/%C3%96zg%C3%BCrl%C3%BCk.mp3"
             },
-            {
-                baslik: "Ben Sana Vurgunum",
-                sanatci: "Ali Kocatepe",
-                sure: "04:00",
-                kapak: "fa-music", 
-                link: "https://senin-cloudflare-r2-linkin.com/odaklanma-sinav.mp3"
-            }
         ]
     },
 	    {
@@ -24,39 +17,208 @@ window.MUZIK_VERITABANI = [
         kategoriIkon: "fa-compact-disc",
         sarkilar: [
             {
-                baslik: "Özgürlük",
-                sanatci: "Zülfü Livaneli",
-                sure: "03:45", 
+                baslik: "Mahalle",
+                sanatci: "Şebnem Ferah",
+                sure: "", 
                 kapak: "fa-music", 
-                link: "https://depo.kirkyama.uk/music/turkish/%C3%96zg%C3%BCrl%C3%BCk.mp3"
+                link: "https://depo.kirkyama.uk/music/turkish/Mahalle.mp3"
             },
             {
-                baslik: "Ben Sana Vurgunum",
-                sanatci: "Ali Kocatepe",
-                sure: "04:00",
+                baslik: "Muğla Zeybeği",
+                sanatci: "Tolga Çandar",
+                sure: "",
                 kapak: "fa-music", 
-                link: "https://senin-cloudflare-r2-linkin.com/odaklanma-sinav.mp3"
-            }
+                link: "https://depo.kirkyama.uk/music/turkish/Mu%C4%9Fla%20Zeybe%C4%9Fi.mp3"
+            },
+			{
+                baslik: "Çiçekler Ekiliyor",
+                sanatci: "Zafer Dilek Orkestrası",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Zafer%20Dilek%20Orkestras%C4%B1%20-%20%C3%87i%C3%A7ekler%20Ekiliyor%20(1976).mp3"
+            },
+			{
+                baslik: "Gurbet",
+                sanatci: "Özemir Erdoğan",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Gurbet%20(1973).mp3"
+            },
+			{
+                baslik: "Fesupanalah",
+                sanatci: "Erkin Koray",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Fesupanallah%20(1977).mp3"
+            },
+			{
+                baslik: "Sarhoş Balık ile Topal Martı",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Sarho%C5%9F%20Bal%C4%B1k%20ile%20Topal%20Mart%C4%B1.mp3"
+            },
+			{
+                baslik: "Eski Arkadaş",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Eski%20Arkada%C5%9F.mp3"
+            },
+			{
+                baslik: "Düşler Sokağı",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/D%C3%BC%C5%9Fler%20Soka%C4%9F%C4%B1.mp3"
+            },
+			{
+                baslik: "Sor Bana Pişman mıyım",
+                sanatci: "Duman",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Duman%20-%20Sor%20Bana%20Pismanmiym.mp3"
+            },
+			{
+                baslik: "Her Seyi Yak",
+                sanatci: "Duman",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Duman%20-%20Her%20Seyi%20Yak.mp3"
+            },
+			{
+                baslik: "Helal Olsun",
+                sanatci: "Duman",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Duman%20-%20Helal%20Olsun.mp3"
+            },
+			{
+                baslik: "Bu Akşam",
+                sanatci: "Duman",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Duman%20-%20Bu%20Aksam.mp3"
+            },
+			{
+                baslik: "Sarhoş Balık ile Topal Martı",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Sarho%C5%9F%20Bal%C4%B1k%20ile%20Topal%20Mart%C4%B1.mp3"
+            },
+			{
+                baslik: "Seni Kendime Sakladım",
+                sanatci: "Duman",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Seni%20Kendime%20Saklad%C4%B1m.MP3"
+            },
+			{
+                baslik: "Siyah Gözler",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Siyah%20G%C3%B6zler.mp3"
+            },
+			{
+                baslik: "Ver Elini",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Ver%20Elini.mp3"
+            },
+
         ]
     },
 	    {
-        kategoriAdi: "Klasik",
+        kategoriAdi: "Slow",
         kategoriIkon: "fa-compact-disc",
         sarkilar: [
             {
-                baslik: "Özgürlük",
-                sanatci: "Zülfü Livaneli",
-                sure: "03:45", 
+                baslik: "Seher Yeli",
+                sanatci: "Erkan Oğur",
+                sure: "", 
                 kapak: "fa-music", 
-                link: "https://depo.kirkyama.uk/music/turkish/%C3%96zg%C3%BCrl%C3%BCk.mp3"
+                link: "https://depo.kirkyama.uk/music/turkish/Seher%20Yeli.mp3"
             },
-            {
+			{
+                baslik: "Siyah Gözler",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Siyah%20G%C3%B6zler.mp3"
+            },
+			{
+                baslik: "Çiçek Abbas",
+                sanatci: "Cahit Berkay",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/%C3%87i%C3%A7ek%20Abbas.mp3"
+            },
+			{
                 baslik: "Ben Sana Vurgunum",
                 sanatci: "Ali Kocatepe",
-                sure: "04:00",
+                sure: "",
                 kapak: "fa-music", 
-                link: "https://senin-cloudflare-r2-linkin.com/odaklanma-sinav.mp3"
-            }
+                link: "https://depo.kirkyama.uk/music/turkish/Ali%20Kocatepe%20-%20Ben%20Sana%20Vurgunum%20(1979).mp3"
+            },
+			{
+                baslik: "Bu Dünya Bir Pencere",
+                sanatci: "Emin İgüs",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Bu%20D%C3%BCnya%20Bir%20Pencere.mp3"
+            },
+			 {
+                baslik: "Yağma Yağmur",
+                sanatci: "Ezginin Günlüğü",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Ya%C4%9Fma%20Ya%C4%9Fmur.mp3"
+            },
+			{
+                baslik: "Bülbülüm Altın Kafeste",
+                sanatci: "Erkan Oğur",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/B%C3%BClb%C3%BCl%C3%BCm%20Alt%C4%B1n%20Kafeste.mp3"
+            },
+			{
+                baslik: "Derdim Çoktur Hangisine Yanayım",
+                sanatci: "Erkan Oğur",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/DERDIM%20COKTUR%20HANGISINE%20YANAYIM.mp3"
+            },
+			{
+                baslik: "Divane Aşık Gibi",
+                sanatci: "Erkan Oğur",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/DIVANE%20ASIK%20GIBI.mp3"
+            },
+			{
+                baslik: "Karşıda Görünen Ne Güzel Yayla",
+                sanatci: "Erkan Oğur",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Kar%C5%9F%C4%B1da%20G%C3%B6r%C3%BCnen%20Ne%20G%C3%BCzel%20Yayla.mp3"
+            },
+			{
+                baslik: "Mecnunum Leylamı Gördüm",
+                sanatci: "Erkan Oğur",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/MECNUNUM%20LEYLAMI%20GORDUM.mp3"
+            },
+			{
+                baslik: "Bir Şuh-i Sitemkar",
+                sanatci: "Erkan Oğur",
+                sure: "",
+                kapak: "fa-music", 
+                link: "https://depo.kirkyama.uk/music/turkish/Bir%20%C5%9Euh-i%20Sitemkar.mp3"
+            },
         ]
     },
 	
@@ -67,14 +229,14 @@ window.MUZIK_VERITABANI = [
             {
                 baslik: "Piramitlerin Gölgesinde",
                 sanatci: "Enstrümantal",
-                sure: "04:21",
+                sure: "",
                 kapak: "fa-music", 
                 link: "https://depo.kirkyama.uk/music/turkish/Ali%20Kocatepe%20-%20Ben%20Sana%20Vurgunum%20(1979).mp3"
             },
             {
                 baslik: "Nil Nehri'nin Sesi",
                 sanatci: "Doğa Sesleri",
-                sure: "10:00",
+                sure: "",
                 kapak: "fa-music", 
                 link: "https://senin-cloudflare-r2-linkin.com/nil-nehri.mp3"
             }
@@ -105,19 +267,12 @@ window.MUZIK_VERITABANI = [
                 kapak: "fa-satellite-dish", 
                 link: "https://knkx-live-a.edge.audiocdn.com/6285_128k"
             },
-            {
-                baslik: "Relaxing Jazz",
-                sanatci: "Lounge & Dinlenme",
-                sure: "Canlı",
-                kapak: "fa-satellite-dish", 
-                link: "https://stream-02-eu.relaxingjazz.com/stream/1/"
-            },
 			{
-                baslik: "Radyo Voyage",
-                sanatci: "Dünya Müzikleri & Etnik",
+                baslik: "Radyo 45'lik",
+                sanatci: "70'ler 80'ler ve 90'lar",
                 sure: "Canlı",
                 kapak: "fa-earth-europe", 
-                link: "https://playerservices.streamtheworld.com/api/livestream-redirect/VOYAGE.mp3"
+                link: "https://stream.radyo45lik.com:4545/stream"
             },
             {
                 baslik: "Borusan Klasik",
@@ -126,6 +281,13 @@ window.MUZIK_VERITABANI = [
                 kapak: "fa-compact-disc", 
                 link: "https://playerservices.streamtheworld.com/api/livestream-redirect/BORUSAN_KLASIK.mp3"
             },
+			{
+                baslik: "Meteoroloji'nin Sesix",
+                sanatci: "Yayın Türü",
+                sure: "Canlı",
+                kapak: "fa-satellite-dish", 
+                link: "https://radyo.yayin.com.tr:6227/stream"
+            },
             {
                 baslik: "Joy Türk",
                 sanatci: "Türkçe Akustik & Slow",
@@ -133,13 +295,7 @@ window.MUZIK_VERITABANI = [
                 kapak: "fa-guitar", 
                 link: "https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_TURK128AAC.aac"
             },
-            {
-                baslik: "Power Pop Akustik",
-                sanatci: "Sakin Türkçe Tınılar",
-                sure: "Canlı",
-                kapak: "fa-microphone-lines", 
-                link: "https://powerpop.listenpowerapp.com/powerpop/mpeg/icecast.audio"
-            }
+			
         ]
 		
     },
